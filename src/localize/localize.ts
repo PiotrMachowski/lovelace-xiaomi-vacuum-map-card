e044875edf967acf6727c7065c2ebdb9d989a527
fbirollo <fbirollo@gmail.com>
2021-11-15 19:30:22 -0300

    Update localize.ts

--- src/localize/localize.ts
+++ src/localize/localize.ts
@@ -1,9 +1,11 @@
 import * as en from "./languages/en.json";
+import * as es from "./languages/es.json";
 import * as pl from "./languages/pl.json";
 import { TranslatableString } from "../types/types";
 
 const languages: Record<string, unknown> = {
     en: en,
+    es: es,
     pl: pl,
 };
 
