a8f8138a18ff0a7816a34964ff1889a4668daca7
LeandroIssa <67451572+LeandroIssa@users.noreply.github.com>
2021-11-15 22:57:12 -0300

    Update localize.ts

--- src/localize/localize.ts
+++ src/localize/localize.ts
@@ -1,10 +1,12 @@
 import * as en from "./languages/en.json";
 import * as pl from "./languages/pl.json";
+import * as pt-br from "./languages/pt-br.json";
 import { TranslatableString } from "../types/types";
 
 const languages: Record<string, unknown> = {
     en: en,
     pl: pl,
+    pt: pt-br,
 };
 
 export function localize(string: string, search = "", replace = ""): string {
