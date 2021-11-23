40a9e91b465d5b659dc0cbbfeca0cc126e22a0da
Kejszijo <smith.zoltan@gmail.com>
2021-11-23 14:03:59 +0100

    Update localize.ts

--- src/localize/localize.ts
+++ src/localize/localize.ts
@@ -9,6 +9,7 @@ import * as pl from "./languages/pl.json";
 import * as ptBr from "./languages/pt-BR.json";
 import * as ru from "./languages/ru.json";
 import * as uk from "./languages/uk.json";
+import * as hu from "./languages/hu.json";
 import { Language, TranslatableString } from "../types/types";
 
 const languages: Record<string, unknown> = {
