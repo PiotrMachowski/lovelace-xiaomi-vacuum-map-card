1bf29de0af1028647f44e44cee16cbfc9550a3d4
mrwogu <mail@wogu.pl>
2022-05-07 01:52:58 +0200

    chore: allow dynamically set serve port

--- rollup.config.dev.js
+++ rollup.config.dev.js
@@ -1,9 +1,12 @@
-import resolve from "rollup-plugin-node-resolve";
 import typescript from "rollup-plugin-typescript2";
+import commonjs from "rollup-plugin-commonjs";
+import nodeResolve from "rollup-plugin-node-resolve";
 import babel from "rollup-plugin-babel";
-import serve from "rollup-plugin-serve";
 import { terser } from "rollup-plugin-terser";
+import serve from "rollup-plugin-serve";
 import json from "@rollup/plugin-json";
+
+const port = process.env.PORT || 5000;
 
 export default {
     input: ["src/xiaomi-vacuum-map-card.ts"],
@@ -12,7 +15,8 @@ export default {
         format: "es",
     },
     plugins: [
-        resolve(),
+        nodeResolve(),
+        commonjs(),
         typescript(),
         json(),
         babel({
@@ -22,7 +26,7 @@ export default {
         serve({
             contentBase: "./dist",
             host: "0.0.0.0",
-            port: 5000,
+            port: port,
             allowCrossOrigin: true,
             headers: {
                 "Access-Control-Allow-Origin": "*",
