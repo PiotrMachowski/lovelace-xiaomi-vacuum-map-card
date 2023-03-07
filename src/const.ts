a7a1ef4c1790024bcacbf66743db49047b4d565e
Piotr Machowski <6118709+PiotrMachowski@users.noreply.github.com>
2023-03-07 17:48:37 +0100

    Set version to v2.1.3-beta

--- src/const.ts
+++ src/const.ts
@@ -1,4 +1,4 @@
-export const CARD_VERSION = "v2.1.2";
+export const CARD_VERSION = "v2.1.3-beta";
 export const CARD_CUSTOM_ELEMENT_NAME = "xiaomi-vacuum-map-card";
 export const EDITOR_CUSTOM_ELEMENT_NAME = "xiaomi-vacuum-map-card-editor";
 export const ACTION_HANDLER_CUSTOM_ELEMENT_NAME = "action-handler-xiaomi-vacuum-map-card";
