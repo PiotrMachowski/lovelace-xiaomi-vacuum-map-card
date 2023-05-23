97939632bbd92548a62f7e7e9bf6b5719748d2cf
Piotr Machowski <PiotrMachowski@users.noreply.github.com>
2023-05-23 04:04:07 +0200

    Extract dropdown renderer

--- src/renderers/modes-menu-renderer.ts
+++ src/renderers/modes-menu-renderer.ts
@@ -1,69 +1,11 @@
-import { css, CSSResultGroup, html, TemplateResult } from "lit";
+import { css, CSSResultGroup, TemplateResult } from "lit";
 
 import { MapMode } from "../model/map_mode/map-mode";
+import { DropdownMenuRenderer } from "./dropdown-menu-renderer";
 
-export class ModesMenuRenderer {
-    public static render(modes: MapMode[], getMode: number, setMode: (number) => void, menu?: HTMLElement): TemplateResult {
-        const getCurrentMode = (): MapMode => modes[getMode];
-        return html`
-            <ha-button-menu class="modes-dropdown-menu" fixed="true" activatable @closed="${(e: Event) => e.stopPropagation()}"
-                @click="${() => ModesMenuRenderer.updateStyles(menu, modes.length)}">
-                <div class="modes-dropdown-menu-button" slot="trigger" alt="bottom align">
-                    <paper-button class="modes-dropdown-menu-button-button">
-                        <ha-icon icon="${getCurrentMode().icon}" class="dropdown-icon"></ha-icon>
-                    </paper-button>
-                    <div class="modes-dropdown-menu-button-text">${getCurrentMode().name}</div>
-                </div>
-                ${modes.map(
-                    (mode, index) => html` <mwc-list-item class="list-item"
-                        ?activated="${getMode === index}"
-                        @click="${(): void => setMode(index)}">
-                        <div class="modes-dropdown-menu-entry clickable ${getMode === index ? "selected" : ""}">
-                            <div
-                                class="modes-dropdown-menu-entry-button-wrapper ${index === 0
-                                    ? "first"
-                                    : ""} ${index === modes.length - 1 ? "last" : ""} ${getMode === index
-                                    ? "selected"
-                                    : ""}">
-                                <paper-button
-                                    class="modes-dropdown-menu-entry-button ${getMode === index ? "selected" : ""}">
-                                    <ha-icon icon="${mode.icon}"></ha-icon>
-                                </paper-button>
-                            </div>
-                            <div class="modes-dropdown-menu-entry-text">${mode.name}</div>
-                        </div>
-                    </mwc-list-item>`,
-                )}
-            </ha-button-menu>
-        `;
-    }
-
-    private static updateStyles(menu: HTMLElement | undefined, items: number): void {
-        const div = menu?.shadowRoot?.querySelector("div") as HTMLElement;
-        if (menu && div) {
-            const height = 50;
-            const minDiff = (items - 1) * height + 32;
-            if (window.innerHeight - div.getBoundingClientRect().bottom >= minDiff) {
-                div.style.marginTop = `0px`;
-                menu.style.marginTop = `0px`;
-                div.style.marginBottom = `-${height}px`;
-                menu.style.marginBottom = `${height}px`;
-            } else {
-                div.style.marginTop = `-${height}px`;
-                menu.style.marginTop = `${height}px`;
-                div.style.marginBottom = "0px";
-                menu.style.marginBottom = "0px";
-            }
-            const mwcMenu = menu.shadowRoot?.querySelector("mwc-menu") as HTMLElement;
-            if (mwcMenu) {
-                mwcMenu.style.zIndex = "1";
-                mwcMenu.style.position = "fixed";
-            }
-            menu.querySelectorAll("mwc-list-item").forEach((item) => {
-                // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-                item.shadowRoot!.querySelector("span")!.style.flexGrow = "1";
-            });
-        }
+export class ModesMenuRenderer extends DropdownMenuRenderer {
+    public static render(modes: MapMode[], getMode: number, setMode: (number) => void, menu: HTMLElement | undefined): TemplateResult {
+        return this._render(modes, getMode, setMode, menu, "modes", true);
     }
 
     public static get styles(): CSSResultGroup {
@@ -81,7 +23,7 @@ export class ModesMenuRenderer {
                 display: inline-flex;
             }
           
-            .list-item:host:host {
+            .modes-dropdown-list-item:host:host {
                 flex-grow: 1;
             }
 
