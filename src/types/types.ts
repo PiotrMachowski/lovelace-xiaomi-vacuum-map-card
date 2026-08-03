2d93e1deea5ef1414f6a046f5b54b1730dc1b7e9
cataseven <52464808+cataseven@users.noreply.github.com>
2026-08-03 23:14:38 +0300

    Add files via upload

--- src/types/types.ts
+++ src/types/types.ts
@@ -63,6 +63,8 @@ export interface CardPresetConfig extends ConditionalObjectConfig {
     readonly map_source: MapSourceConfig;
     readonly map_locked?: boolean;
     readonly two_finger_pan?: boolean;
+    readonly map_rotation?: number;
+    readonly map_rotation_gesture?: boolean;
     readonly calibration_source?: CalibrationSourceConfig;
     readonly icons?: IconActionConfig[];
     readonly append_icons?: boolean;
